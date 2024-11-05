6d2ee23e3335d77b70dadd054970e9c2c5cab3df
hamid <-->
2024-11-05 05:16:45 +0100

    vim portal extension

---
+++ vim-portal/extension.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const disposable = vscode.commands.registerCommand('hellohamidid.helloWorld', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const fileName = document.fileName;
+			const cursorPosition = editor.selection.active;
+			const lineNumber = cursorPosition.line + 1;
+			const columnNumber = cursorPosition.character + 1;
+			//const cmd = `nvim +${lineNumber} ${fileName}`;
+			const cmd = `nvim "+call cursor(${lineNumber},${columnNumber})" "${fileName}"`;
+			vscode.window.showInformationMessage(cmd);
+			const terminalName = 'NeoVim Terminal';
+			let terminal = vscode.window.terminals.find(t => t.name === terminalName);
+			if (!terminal) {
+				terminal = vscode.window.createTerminal(terminalName);
+			}
+			terminal.show();
+			terminal.sendText(cmd);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
